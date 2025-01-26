bf741bc6ede223c2f375e6f8a2e66bfb6852a233
KhalidELSwerky <Kjmal3839@gmail.com>
2025-01-26 06:05:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,236 @@
+let name = document.getElementById('name');
+let gba  = document.getElementById('gba');
+let id   = document.getElementById('id');
+let material1 = document.getElementById('material1');
+let material2 = document.getElementById('material2');
+let material3 = document.getElementById('material3');
+let material4 = document.getElementById('material4');
+let material5 = document.getElementById('material5');
+let material6 = document.getElementById('material6');
+let material7 = document.getElementById('material7');
+let input = document.getElementById('input');
+let but = document.getElementById('but');
+let con = document.getElementById('con') ;
+
+con.style.display='none'
+function result(){
+   
+    if(input.value =='' || isNaN(input.value)||input.value.length !=5)
+        {
+        window.alert("يرجى إدخال رقم صحيح مكون من 5 أرقام");
+        
+        
+      }
+    else if(input.value==11057){
+        
+        name.innerHTML +='khalid gamal ahmed';
+       let datasince=40;
+       let operatingsystem=45;
+       let network=89;
+       let marketing=89;
+       let information=89;
+       let acounting=89;
+        id.innerHTML +=input.value;
+       let mat1 = material1.innerHTML += datasince;
+       let mat2 = material2.innerHTML += operatingsystem;
+       let mat3 = material3.innerHTML +=network;
+       let mat4 = material4.innerHTML +=marketing;
+       let mat5 = material5.innerHTML +=information;
+       let mat6 = material6.innerHTML +=acounting; 
+       gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+       input.value=''
+       con.style.display='block';
+
+       
+       
+    }
+    else if(input.value==11050){
+        name.innerHTML +='elmotasem hassan ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value=''
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11051){
+        name.innerHTML +='elsayed mohammed';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value=''
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11052){
+        name.innerHTML +='mohamed kareem ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value=''
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11053){
+        name.innerHTML +='elragehy ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value=''
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11054){
+        name.innerHTML +='emad ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value='';
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11055){
+        name.innerHTML +='kareem ahmed ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value='';
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11056){
+        name.innerHTML +='trabees ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=89;
+        let marketing=89;
+        let information=89;
+        let acounting=89;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value=''
+        con.style.display='block';
+    }
+
+    
+    else if(input.value==11058){
+        name.innerHTML +='abd ullah gamal hmed ';
+       
+        let datasince=75;
+        let operatingsystem=89;
+        let network=70;
+        let marketing=69;
+        let information=80;
+        let acounting=84;
+         id.innerHTML +=input.value;
+        let mat1 = material1.innerHTML += datasince;
+        let mat2 = material2.innerHTML += operatingsystem;
+        let mat3 = material3.innerHTML +=network;
+        let mat4 = material4.innerHTML +=marketing;
+        let mat5 = material5.innerHTML +=information;
+        let mat6 = material6.innerHTML +=acounting;
+        gpa.innerHTML +=((datasince+ operatingsystem+ network+ marketing+ information+ acounting)/600)*4;
+        input.value=''
+        con.style.display='block';
+    }
+
+    
+
+
+    else{
+        alert("رقم الجلوس الخاص بك اما خطا او ان النتيجه لم ترفع بعد");
+        location.reload()
+
+
+    }
+
+
+    }
+    
+    
+
